2ab1896832434c23109049c56e74c764ee418882
MING-beta <ch345300@gmail.com>
2026-02-26 12:57:39 +0900

    chore: ?먮룞 ???而ㅻ컠 (Auto Save)

--- auto_commit.bat
+++ auto_commit.bat
@@ -5,7 +5,7 @@ cd /d "C:\Users\user\Desktop\dopaming\fire-calculator"
 :loop
 echo 작업 내역을 GitHub에 자동 업로드 합니다...
 git add .
-git commit -m "자동 저장 커밋"
+git commit -m "chore: 자동 저장 커밋 (Auto Save)"
 git push origin main
 
 echo 완료! 10분 뒤에 다시 실행됩니다.
